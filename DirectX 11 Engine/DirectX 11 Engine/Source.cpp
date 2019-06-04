a7dbaded114bbf95ca5e5ba450087d6ed4784af1
Xavier <Oliver>
2019-06-04 15:38:41 +0100

    Hello World

---
+++ DirectX 11 Engine/DirectX 11 Engine/Source.cpp
@@ -0,0 +1,24 @@
+#include <d3d11.h>
+#include <dxgi.h>
+#include <windows.h>
+#include <tchar.h>
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmd, int nCmdShow)
+{
+	D3D11CreateDeviceAndSwapChain(NULL,
+		D3D_DRIVER_TYPE_REFERENCE,
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		D3D11_SDK_VERSION,
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		NULL);
+
+	MessageBoxA(NULL, "Hello World!", "[Game Engine]", MB_OK);
+
+	return 0;
+}
